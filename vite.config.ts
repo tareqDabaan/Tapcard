3c206efcb0eb6f2519024e7e7c5be0bfd0535697
Tareq Dabaan <81941114+tareqDabaan@users.noreply.github.com>
2026-01-28 07:50:50 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/the-coffee-mode-salmiya-showcase/",
+  base: "/Tapcard/",
   server: {
     host: "::",
     port: 8080,
